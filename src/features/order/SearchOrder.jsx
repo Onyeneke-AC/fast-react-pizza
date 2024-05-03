602f6837091df38516c505a15efb208831603bd5
Onyeneke Anthony Chidubem <98026459+Onyeneke-AC@users.noreply.github.com>
2024-05-03 22:38:49 +0100

    styling form elements

--- src/features/order/SearchOrder.jsx
+++ src/features/order/SearchOrder.jsx
@@ -20,6 +20,7 @@ function SearchOrder() {
         value={query}
         onChange={(e) => setQuery(e.target.value)}
         placeholder="Search Order #"
+        className="w-28 rounded-full bg-yellow-100 px-4 py-2 text-sm transition-all duration-300 placeholder:text-stone-400 focus:outline-none focus:ring focus:ring-yellow-500 sm:w-64 sm:focus:w-72"
       />
     </form>
   );
