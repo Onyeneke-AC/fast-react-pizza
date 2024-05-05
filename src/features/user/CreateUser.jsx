03a8eb63948d5928ccb369ae2acde8d23b8ef0d0
Onyeneke Anthony Chidubem <98026459+Onyeneke-AC@users.noreply.github.com>
2024-05-05 23:16:41 +0100

    Modeling the 'user' state with redux

--- src/features/user/CreateUser.jsx
+++ src/features/user/CreateUser.jsx
@@ -1,11 +1,20 @@
 import { useState } from "react";
 import Button from "../../ui/Button";
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { updateName } from "./userSlice";
 
 function CreateUser() {
   const [username, setUsername] = useState("");
+  // const navigate = useNavigate();
+  // const dispatch = useDispatch();
 
   function handleSubmit(e) {
     e.preventDefault();
+
+    // dispatch(updateName(username));
+
+    // navigate("/menu");
   }
 
   return (
